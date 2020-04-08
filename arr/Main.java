6b0db37248fd8b12a13af29ed2b5bf39b428d94c
LeeHyengTae <tedd911@icloud.com>
2020-04-08 20:20:35 +0900

    20:20

--- arr/Main.java
+++ arr/Main.java
@@ -1,20 +1,21 @@
 package com.jse.arr;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
 		CardService service = new CardService();
 		while(true) {
-			switch(JOptionPane.showInputDialog("0.Exit  1.Go  2.카드 3장 받기  3.받은 카드 출력")) {
-			case "0": return;
-			case "1": System.out.println("Go");
+			System.out.println("0.Exit  1.Go  2.카드 3장 받기  3.받은 카드 출력");
+			switch(scanner.nextInt()) {
+			case 0: return;
+			case 1: System.out.println("Go");
 					break;
-			case "2": System.out.println("카드 3장 받기");
+			case 2: System.out.println("카드 3장 받기");
 			for(int i=0; i<3; i++) {
-				String[] values = JOptionPane.
+				
 				System.out.println("카드무늬, 카드넘버 입력");
 				service.add(new CardBean(scanner.next(), scanner.nextInt()));				
 			}
